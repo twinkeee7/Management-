d946bb03b84eb8e9eb93f8a87ee20e12787ebe0a
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-05-10 18:17:56 +0530

    Update screenshot.py fix

--- X/modules/user/screenshot.py
+++ X/modules/user/screenshot.py
@@ -45,9 +45,9 @@ async def screenshot(bot: Client, message: Message):
         message.delete(),
         bot.invoke(
             functions.messages.SendScreenshotNotification(
-                peer=await X.resolve_peer(message.chat.id),
+                peer=await Client.resolve_peer(message.chat.id),
                 reply_to_msg_id=0,
-                random_id=X.rnd_id(),
+                random_id=Client.rnd_id(),
             )
         ),
     )
