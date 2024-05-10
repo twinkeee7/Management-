32023032edb7966f9d0fdfd157bcba8b1ae3a4ae
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-05-10 18:37:18 +0530

    Update user

---
+++ X/modules/user/slowspam.py
@@ -0,0 +1,49 @@
+#MIT License
+
+#Copyright (c) 2024 Japanese-X-Userbot
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+# Credits: NOBITA XD AND KUNAL 
+# Copyright (C) 2024 JAPANESE X USERBOT AND STORM USERBOT 
+#DON'T KANG FUCKING COWARD
+#BSDKE KANG KIYA TOH SOCH LIYO
+#AAG LAGA DUNGA TERE ANDAR 
+#SAMJHA ? 
+
+
+
+import asyncio
+from threading import Event
+
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+
+from config import BLACKLIST_CHAT, BOTLOG_CHATID
+from config import CMD_HANDLER
+from config import SUDO_USERS
+from X.helpers.basic import edit_or_reply
+from X.utils.misc import extract_args
+
+from .help import *
+
+
+
+
+
