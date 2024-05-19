950e2b67db493cc4f94977ceedc70677b8527d12
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-05-19 19:54:59 +0530

    Create wikipedia.py

---
+++ X/modules/user/wikipedia.py
@@ -0,0 +1,53 @@
+#MIT License
+
+#Copyright (c) 2024 Japanese-X-Userbot
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+
+import wikipedia
+from uniborg.util import fire_on_cmd
+
+from config import SUDO_USERS
+from .help import *
+
+@Client.on_message(
+    filters.command(["wiki"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Processing ...")
+    input_str = event.pattern_match.group(1)
+    result = ""
+    results = wikipedia.search(input_str)
+    for s in results:
+        page = wikipedia.page(s)
+        url = page.url
+        result += f"> [{s}]({url}) \n"
+    await event.edit(
+        "WikiPedia **Search**: {} \n\n **Result**: \n\n{}".format(input_str, result)
+      )
+
+  add_command_help(
+    "•─╼⃝𖠁 Wɪᴋɪᴘᴇᴅɪᴀ",
+    [
+        ["wiki", "Tᴏ Wɪᴋɪᴘᴇᴅɪᴀ ɪɴ ᴛʜɪꜱ ᴄʜᴀᴛ."],
+    ],
+  )
