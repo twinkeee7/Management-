9b323a8f6e3161ce0520455fba9f71deb4bc4e8d
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-05-31 19:03:35 +0530

    Update naruto.py

--- X/modules/user/naruto.py
+++ X/modules/user/naruto.py
@@ -20,4 +20,34 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#coming soon
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["naruto"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def naruto(client, message):
+    args = message.text.split(" ")[1:]
+    narutodata = [
+  "https://graph.org/file/8fce7cc2772cc1019bae7.jpg",
+  "https://graph.org/file/2d2c05e2aff362cbf53b5.jpg",
+  "https://graph.org/file/462819a7ec630262e707d.jpg",
+  "https://graph.org/file/10df5c17f1b54de0e0b3a.jpg",
+  "https://graph.org/file/556ab5451f992560bd443.jpg",
+  "https://graph.org/file/f8790f64533dedddd6aeb.jpg",
+  "https://graph.org/file/0dbd7f3b7683ce2d9a8cf.jpg",
+  "https://graph.org/file/b6c2f34cd985a2d65b08a.jpg",
+    ]
+  naruto_url = random.choice(narutodata)
+    await message.reply_photo(naruto_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Nᴀʀᴜᴛᴏ",
+    [
+       ["naruto", "Gɪᴠᴇ random Nᴀʀᴜᴛᴏ pic."],
+        ],
+)
