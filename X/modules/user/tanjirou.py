77244a45242300bd9a692f9e4384c9ad3292ddae
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-01 12:03:36 +0530

    Update tanjirou.py

--- X/modules/user/tanjirou.py
+++ X/modules/user/tanjirou.py
@@ -20,4 +20,34 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#coming soon 
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["tanjirou"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def demon(client, message):
+    args = message.text.split(" ")[1:]
+    demondata = [
+  "https://graph.org/file/a484e26bbd9b6f8757426.jpg",
+  "https://graph.org/file/caea011b326ecf637fcda.jpg",
+  "https://graph.org/file/2673b2b68f63ad7483f5b.jpg",
+  "https://graph.org/file/ec50f6a102ad36411f70e.jpg",
+  "https://graph.org/file/fddc28fc2e8d856f572c4.jpg",
+  "https://graph.org/file/fddc28fc2e8d856f572c4.jpg",
+  "https://graph.org/file/8b635492173f1541c7d3b.jpg",
+  "https://graph.org/file/2673b2b68f63ad7483f5b.jpg",
+    ]
+    demon_url = random.choice(demondata)
+    await message.reply_photo(demon_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Kᴀᴍᴀᴅᴏ Tᴀɴᴊɪʀᴏᴜ",
+    [
+       ["tanjirou", "Gɪᴠᴇ random Kᴀᴍᴀᴅᴏ Tᴀɴᴊɪʀᴏᴜ Sʟᴀʏᴇʀ pic."],
+        ],
+)
