1cb00abe4cde9997009dc9b3d64d6395a58798c4
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-01 10:05:06 +0530

    Update nezuko.py

--- X/modules/user/nezuko.py
+++ X/modules/user/nezuko.py
@@ -20,4 +20,34 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#coming soon
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["nezuko"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def nezuko(client, message):
+    args = message.text.split(" ")[1:]
+    nezukodata = [
+  "https://graph.org/file/55103e5cb0177f358cf58.jpg",
+  "https://graph.org/file/413a0286008fe951e68ba.jpg",
+  "https://graph.org/file/9e590e50dc1608d27d581.jpg",
+  "https://graph.org/file/2c622c6e77057c85463e7.jpg",
+  "https://graph.org/file/1a14904bc7465b25965d5.jpg",
+  "https://graph.org/file/8f63eada971a6eb82307c.jpg",
+  "https://graph.org/file/54a295caff124bc28bbff.jpg",
+  "https://graph.org/file/55103e5cb0177f358cf58.jpg",
+    ]
+    nezuko_url = random.choice(nezukodata)
+    await message.reply_photo(nezuko_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Nᴇᴢᴜᴋᴏ",
+    [
+       ["nezuko", "Gɪᴠᴇ random Nᴇᴢᴜᴋᴏ pic."],
+        ],
+  )
