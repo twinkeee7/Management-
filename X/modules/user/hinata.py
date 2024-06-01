a722ed889b1fcee6c4c5a95990a8ae4d0d5bb76a
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-01 11:44:52 +0530

    Update hinata.py

--- X/modules/user/hinata.py
+++ X/modules/user/hinata.py
@@ -20,4 +20,34 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#coming soon 
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["hinata"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def hinata(client, message):
+    args = message.text.split(" ")[1:]
+    hinatadata = [
+  "https://graph.org/file/6851f728d2378426c543b.jpg",
+  "https://graph.org/file/86059f15b7345f135d303.jpg",
+  "https://graph.org/file/94d6c15b9fc6d92e8b5c8.jpg",
+  "https://graph.org/file/12d3f519481a086dfabb4.jpg",
+  "https://graph.org/file/a636ffa711ed3fe1148fe.jpg",
+  "https://graph.org/file/7b9d3b1028712377569b6.jpg",
+  "https://graph.org/file/17c301cfd5dc72c2ec4a1.jpg",
+  "https://graph.org/file/f3a3d3065606acf2a7c12.jpg",
+    ]
+    hinata_url = random.choice(hinatadata)
+    await message.reply_photo(hinata_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Hɪɴᴀᴛᴀ",
+    [
+       ["hinata", "Gɪᴠᴇ random Hɪɴᴀᴛᴀ pic."],
+        ],
+)
