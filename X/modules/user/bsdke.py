90b4910f4037e293fc6ea6a8355028574dbf33cb
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-02 22:45:48 +0530

    Update bsdke.py

--- X/modules/user/bsdke.py
+++ X/modules/user/bsdke.py
@@ -19,3 +19,37 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+#Legnendx22 Thanks for your Sticker pack 
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["bsdke"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def bsdke(client, message):
+    args = message.text.split(" ")[1:]
+    bsdkedata = [
+  "https://graph.org/file/ac866e1d75bdfdb1e2edf.jpg",
+  "https://graph.org/file/347eedd9b939c57776f97.jpg",
+  "https://graph.org/file/2e51c1cb2ebfaf5b599e0.jpg",
+  "https://graph.org/file/a7584d179f380b5627485.jpg",
+  "https://graph.org/file/54a3e66e3c30e340cb1fa.jpg",
+  "https://graph.org/file/76575287d9fa600d5effb.jpg",
+  "https://graph.org/file/a27baae47423663239076.jpg",
+  "https://graph.org/file/675a9904801c5bd43bc60.jpg",
+    ]
+    bsdke_url = random.choice(bsdkedata)
+    await message.reply_photo(bsdke_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Bsᴅᴋᴇ",
+    [
+       ["bsdke", "Gɪᴠᴇ random Bsᴅᴋᴇ pic."],
+        ],
+)
