5958c03fbfb9e27b89f5801e28310b320206a414
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-03 09:21:04 +0530

    Update autoforward.py

--- X/modules/user/autoforward.py
+++ X/modules/user/autoforward.py
@@ -20,4 +20,9 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#coming soon 
+from asyncio import sleep
+from logging import getLogger
+
+from pyrogram import filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
