17c91ef528716a6ebc0515ef30b182e102c7a5de
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-04 23:05:11 +0530

    Update goodmorning.py

--- X/modules/user/goodmorning.py
+++ X/modules/user/goodmorning.py
@@ -19,3 +19,36 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["morning"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def morning(client, message):
+    args = message.text.split(" ")[1:]
+    morningdata = [
+  "https://graph.org/file/2df01555304e071e6ef55.jpg",
+  "https://graph.org/file/d2404ca3ad18f19db4788.jpg",
+  "https://graph.org/file/de8d9f80ab98d6af0b00f.jpg",
+  "https://graph.org/file/a0ab477db07d2ebad5c5b.jpg",
+  "https://graph.org/file/a0ab477db07d2ebad5c5b.jpg",
+  "https://graph.org/file/de8d9f80ab98d6af0b00f.jpg",
+  "https://graph.org/file/d2404ca3ad18f19db4788.jpg",
+  "https://graph.org/file/2df01555304e071e6ef55.jpg",
+    ]
+    morning_url = random.choice(morningdata)
+    await message.reply_photo(morning_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Gᴏᴏᴅ Mᴏʀɴɪɴɢ",
+    [
+       ["morning", "Gɪᴠᴇ random Gᴏᴏᴅ Mᴏʀɴɪɴɢ pic."],
+        ],
+)
