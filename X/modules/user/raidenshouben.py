93a729a6c51993cb629acdbe43f961848b6cf6a0
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-04 20:56:51 +0530

    Update raidenshouben.py Rᴀɪᴅᴇɴ Sʜᴏᴜʙᴇɴ

--- X/modules/user/raidenshouben.py
+++ X/modules/user/raidenshouben.py
@@ -19,3 +19,36 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["raiden"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def hinata(client, message):
+    args = message.text.split(" ")[1:]
+    hinatadata = [
+  "https://graph.org/file/eb0446474b90145fe1dc0.jpg",
+  "https://graph.org/file/b7a31e8712cd6ca1e6c69.jpg",
+  "https://graph.org/file/ac29896bf3f601ad47c6e.jpg",
+  "https://graph.org/file/c0770646fd3b3f8ac92af.jpg",
+  "https://graph.org/file/a54bd529c2a53c9b8b063.jpg",
+  "https://graph.org/file/3175fc67a421991e18200.jpg",
+  "https://graph.org/file/eb0446474b90145fe1dc0.jpg",
+  "https://graph.org/file/ac29896bf3f601ad47c6e.jpg",
+    ]
+    hinata_url = random.choice(hinatadata)
+    await message.reply_photo(hinata_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Rᴀɪᴅᴇɴ Sʜᴏᴜʙᴇɴ",
+    [
+       ["raiden", "Gɪᴠᴇ random Rᴀɪᴅᴇɴ Sʜᴏᴜʙᴇɴ pic."],
+        ],
+)
