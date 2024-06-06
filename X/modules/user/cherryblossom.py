794a0d4a44241538fc372e8ef44511d6ed122110
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-06 15:32:08 +0530

    Update cherryblossom.py

--- X/modules/user/cherryblossom.py
+++ X/modules/user/cherryblossom.py
@@ -19,3 +19,35 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["blossom"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def castle(client, message):
+    args = message.text.split(" ")[1:]
+    castledata = [
+  "https://graph.org/file/4ba6fb292fedd6e071996.jpg",
+  "https://graph.org/file/dbea429cbd1644a800f62.jpg",
+  "https://graph.org/file/b0b8a357cb7dadfb7e6af.jpg",
+  "https://graph.org/file/3b1059f85660ed5be4092.jpg",
+  "https://graph.org/file/2dec9a542b29deab036ff.jpg",
+  "https://graph.org/file/319a8a766f32a9f802e08.jpg",
+  "https://graph.org/file/fc0d9364d736ab555f043.jpg",
+  "https://graph.org/file/79ec28c9e9deb6181067d.jpg",
+    ]
+    castle_url = random.choice(castledata)
+    await message.reply_photo(castle_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Cʜᴇʀʀʏ Bʟᴏssᴏᴍ",
+    [
+       ["blossom", "Gɪᴠᴇ random Cʜᴇʀʀʏ Bʟᴏssᴏᴍ pic."],
+        ],
+)
