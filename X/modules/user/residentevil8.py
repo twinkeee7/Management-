7cb7add294988103d7b9f493f9d8338903c45da2
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-07 17:38:52 +0530

    Update residentevil8.py

--- X/modules/user/residentevil8.py
+++ X/modules/user/residentevil8.py
@@ -19,3 +19,35 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["rs8"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def castle(client, message):
+    args = message.text.split(" ")[1:]
+    castledata = [
+  "https://graph.org/file/78560fa0a4f6443114ac2.jpg",
+  "https://graph.org/file/20cf8b023c71ef69385f3.jpg",
+  "https://graph.org/file/41cccc37d87b6c7762c90.jpg",
+  "https://graph.org/file/20cf8b023c71ef69385f3.jpg",
+  "https://graph.org/file/f72e5e311cfedd3304fd0.jpg",
+  "https://graph.org/file/e2d1a91ba4f0329313d0f.jpg",
+  "https://graph.org/file/d514871860235ddbc49a1.jpg",
+  "https://graph.org/file/2936bf39aea5cf17febb0.jpg",
+    ]
+    castle_url = random.choice(castledata)
+    await message.reply_photo(castle_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Rᴇsɪᴅᴇɴᴛ ᴇᴠɪʟ 8",
+    [
+       ["rs8", "Gɪᴠᴇ random Rᴇsɪᴅᴇɴᴛ ᴇᴠɪʟ 8 pic."],
+        ],
+)
