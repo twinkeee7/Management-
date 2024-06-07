6c58435e4565af22ca7e2177c66af98b4f1af858
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-07 17:29:27 +0530

    Update residentevil4remake.py

--- X/modules/user/residentevil4remake.py
+++ X/modules/user/residentevil4remake.py
@@ -19,3 +19,35 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["rs4"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def castle(client, message):
+    args = message.text.split(" ")[1:]
+    castledata = [
+  "https://graph.org/file/d326009f34a278f2eaefa.jpg",
+  "https://graph.org/file/292694369e9c2d6ad56ad.jpg",
+  "https://graph.org/file/f1b8cf4a6ab67c63f1700.jpg",
+  "https://graph.org/file/28816e1bab602fe780bdf.jpg",
+  "https://graph.org/file/7560c6d0590223fe34c2d.jpg",
+  "https://graph.org/file/9f194858f64904bb675df.jpg",
+  "https://graph.org/file/d326009f34a278f2eaefa.jpg",
+  "https://graph.org/file/292694369e9c2d6ad56ad.jpg",
+    ]
+    castle_url = random.choice(castledata)
+    await message.reply_photo(castle_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Rᴇsɪᴅᴇɴᴛ ᴇᴠɪʟ 4 ʀᴇᴍᴀᴋᴇ",
+    [
+       ["rs4", "Gɪᴠᴇ random Rᴇsɪᴅᴇɴᴛ ᴇᴠɪʟ 4 ʀᴇᴍᴀᴋᴇ pic."],
+        ],
+)
