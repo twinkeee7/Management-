7b89a156cc4131ba01408eef2afe87c53f3fc027
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-07 16:33:43 +0530

    Update mountfuji.py Mᴏᴜɴᴛ Fᴜᴊɪ

--- X/modules/user/mountfuji.py
+++ X/modules/user/mountfuji.py
@@ -19,3 +19,36 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["fuji"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def castle(client, message):
+    args = message.text.split(" ")[1:]
+    castledata = [
+  "https://graph.org/file/35424f110fac7acfad730.jpg",
+  "https://graph.org/file/62dc1c72a59522fd61000.jpg",
+  "https://graph.org/file/e76dcebdce2c126b682cd.jpg",
+  "https://graph.org/file/bbaaf348c363ae0167ef4.jpg",
+  "https://graph.org/file/0008dc21964d360b0ca20.jpg",
+  "https://graph.org/file/166efa15e7559440e11fb.jpg",
+  "https://graph.org/file/da1fb683140e9d5eb79b2.jpg",
+  "https://graph.org/file/62dc1c72a59522fd61000.jpg",
+    ]
+    castle_url = random.choice(castledata)
+    await message.reply_photo(castle_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Mᴏᴜɴᴛ Fᴜᴊɪ",
+    [
+       ["fuji", "Gɪᴠᴇ random Mᴏᴜɴᴛ Fᴜᴊɪ pic."],
+        ],
+)
