10dc27fee11af8005b925bce1d51f1f3cd72322b
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-07 16:38:59 +0530

    Update osaka.py

--- X/modules/user/osaka.py
+++ X/modules/user/osaka.py
@@ -19,3 +19,36 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["osaka"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def castle(client, message):
+    args = message.text.split(" ")[1:]
+    castledata = [
+  "https://graph.org/file/d1f9bf6ed087bd52305f2.jpg",
+  "https://graph.org/file/9a9852c876b51267729eb.jpg",
+  "https://graph.org/file/d00a1849c16271abcc787.jpg",
+  "https://graph.org/file/8d84cc22b365cca74ea54.jpg",
+  "https://graph.org/file/2c0cf62508006d18b5c11.jpg",
+  "https://graph.org/file/9fd072388b0b4ca9833ce.jpg",
+  "https://graph.org/file/53cdcda9ff5af53a993b1.jpg",
+  "https://graph.org/file/d1f9bf6ed087bd52305f2.jpg",
+    ]
+    castle_url = random.choice(castledata)
+    await message.reply_photo(castle_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Osᴀᴋᴀ",
+    [
+       ["osaka", "Gɪᴠᴇ random Osᴀᴋᴀ pic."],
+        ],
+)
