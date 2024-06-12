608d676259df483f049c06794e6398011e71ee1e
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-12 12:55:47 +0530

    Update kasukabe.py #126

--- X/modules/user/kasukabe.py
+++ X/modules/user/kasukabe.py
@@ -19,3 +19,35 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["kasukabe"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def hinata(client, message):
+    args = message.text.split(" ")[1:]
+    hinatadata = [
+  "https://graph.org/file/121d78924750d539de018.jpg",
+  "https://graph.org/file/aaa4b76dc13e37ca23783.jpg",
+  "https://graph.org/file/227228607397aefe1f0da.jpg",
+  "https://graph.org/file/cb755e82d3d4fa74e5e8a.jpg",
+  "https://graph.org/file/cc314e6499f0ae72e3a0b.jpg",
+  "https://graph.org/file/5e9722fdf1cc21f7847a8.jpg",
+  "https://graph.org/file/aaa4b76dc13e37ca23783.jpg",
+  "https://graph.org/file/121d78924750d539de018.jpg",
+    ]
+    hinata_url = random.choice(hinatadata)
+    await message.reply_photo(hinata_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Kᴀsᴜᴋᴀʙᴇ",
+    [
+       ["kasukabe", "Gɪᴠᴇ random Kᴀsᴜᴋᴀʙᴇ pic."],
+        ],
+)
