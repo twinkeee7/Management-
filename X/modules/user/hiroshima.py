ccfb284e3556fd8488d9fac51af703864c232942
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-12 13:04:50 +0530

    Update hiroshima.py Hɪʀᴏsʜɪᴍᴀ

--- X/modules/user/hiroshima.py
+++ X/modules/user/hiroshima.py
@@ -19,3 +19,35 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+import random
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from .help import * 
+
+hl = "."
+
+@Client.on_message(
+    filters.command(["hiroshima"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def hinata(client, message):
+    args = message.text.split(" ")[1:]
+    hinatadata = [
+  "https://graph.org/file/f6d545de22d75795850df.jpg",
+  "https://graph.org/file/f3a25c75cdf4e157ae73c.jpg",
+  "https://graph.org/file/a227f1de448ea4842b8f3.jpg",
+  "https://graph.org/file/bdbdb35b7202fcba1003d.jpg",
+  "https://graph.org/file/ebc4f511d30ab10b9aa05.jpg",
+  "https://graph.org/file/a64c485fdc99588e421b8.jpg",
+  "https://graph.org/file/a227f1de448ea4842b8f3.jpg",
+  "https://graph.org/file/f3a25c75cdf4e157ae73c.jpg",
+    ]
+    hinata_url = random.choice(hinatadata)
+    await message.reply_photo(hinata_url)
+
+add_command_help(
+    "•─╼⃝𖠁 Hɪʀᴏsʜɪᴍᴀ",
+    [
+       ["hiroshima", "Gɪᴠᴇ random Hɪʀᴏsʜɪᴍᴀ pic."],
+        ],
+)
