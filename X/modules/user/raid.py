34054b4554234102a2b9ae09536ed2ae05807b7f
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-06-12 15:28:43 +0530

    Update raid.py

--- X/modules/user/raid.py
+++ X/modules/user/raid.py
@@ -27,9 +27,6 @@
 #AAG LAGA DUNGA TERE ANDAR 
 #SAMJHA ? 
 
-
-
-
 import asyncio
 from random import choice
 from pyrogram.types import Message
@@ -37,7 +34,9 @@ from pyrogram import filters, Client
 from config import OWNER_ID
 from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
+from config import OWNER_ID, SUDO_USERS, CMD_HANDLER as cmd
 from XDB.data import RAID
+from XDB.data import RAID, MASTERS
 from .help import *
 
 @Client.on_message(
@@ -67,6 +66,59 @@ async def raid(x: Client, e: Message):
 
       else:
             await e.reply_text(".Rᴀɪᴅ 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+
+reply_raid_users = []
+
+@Client.on_message(filters.command(["rraid", "replyraid"], cmd) & (filters.me | filters.user(SUDO_USERS)))
+async def activate_reply_raid(client: Client, message: Message):
+    global reply_raid_users
+    args = message.text.split()
+
+    if len(args) > 1:
+        user = await client.get_users(args[1])
+        user_id = user.id
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        await message.reply_text(f"{cmd}rraid <username> or reply to a user")
+        return
+
+    if user_id in MASTERS:
+        await message.reply_text("Nope, this guy is an owner ☠️")
+    elif user_id == OWNER_ID:
+        await message.reply_text("Nope, this guy is the owner of these bots 🥀")
+    elif user_id in SUDO_USERS:
+        await message.reply_text("Nope, this guy is a sudo user 💗")
+    else:
+        reply_raid_users.append(user_id)
+        await message.reply_text("Activated reply raid ✅")
+
+@Client.on_message(filters.command(["drraid", "draid", "dreplyraid"], cmd) & (filters.me | filters.user(SUDO_USERS)))
+async def deactivate_reply_raid(client: Client, message: Message):
+    global reply_raid_users
+    args = message.text.split()
+
+    if len(args) > 1:
+        user = await client.get_users(args[1])
+        user_id = user.id
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        await message.reply_text(f"{cmd}drraid <username> or reply to a user")
+        return
+
+    if user_id in reply_raid_users:
+        reply_raid_users.remove(user_id)
+        await message.reply_text("Reply raid de-activated ✅")
+
+@Client.on_message(~filters.me & filters.incoming)
+async def reply_raid_watcher(client: Client, message: Message):
+    global reply_raid_users
+    user_id = message.from_user.id
+
+    if user_id in reply_raid_users:
+        reply_text = choice(RAID)
+        await message.reply_text(reply_text)
 
 
 add_command_help(
