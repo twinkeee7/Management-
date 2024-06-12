bd4cc86af3e9089c67779440ada720b2a6b1cfc8
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-06-12 13:11:00 +0530

    Update replyraid.py

--- X/modules/user/replyraid.py
+++ X/modules/user/replyraid.py
@@ -19,3 +19,12 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+
+
+import asyncio
+from random import choice
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from XDB.data import MASTERS, RAID
+from config import SUDO_USERS, OWNER_ID
