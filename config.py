1f66ae7ada2a4ce80c97e732c851f05c73a65056
twinkeee7 <167674604+twinkeee7@users.noreply.github.com>
2024-06-15 10:30:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,9 +30,9 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 ALIVE_EMOJI = getenv("ALIVE_EMOJI", "🥵😂")
-ALIVE_LOGO = getenv("ALIVE_LOGO", "https://graph.org/file/ec99cb6dba229bd984537.jpg")
-PM_PIC = getenv("PM_PIC", "https://graph.org/file/936ef33023a77fa9a6813.jpg")
-ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://graph.org/file/ac1104e0d5301989bc71b.jpg")
+PM_PIC = getenv("PM_PIC", "https://graph.org/file/13c921143d0a029e65be2.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey RiseMembers, I am alive.")
 API_HASH = getenv("API_HASH", "34efb38c74d5e6b25d1bb6234396a8af")
 API_ID = getenv("API_ID", "23129036")
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
@@ -41,16 +41,16 @@ BRANCH = getenv("BRANCH", "main") #don't change this line
 CMD_HNDLR = cmd
 BOT_TOKEN = getenv("BOT_TOKEN", "none")
 OPENAI_API_KEY = getenv("OPENAI_API_KEY", "")
-CHANNEL = getenv("CHANNEL", "Japanese_Userbot")
+CHANNEL = getenv("CHANNEL", "@Riseeuniversee")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
-GROUP = getenv("GROUP", "Japanese_Userbot_Support")
+GROUP = getenv("GROUP", "@Riseeuniversee")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
-REPO_URL = getenv("REPO_URL", "https://github.com/Team-Japanese/Japanese-X-Userbot")
+REPO_URL = getenv("REPO_URL", "https://t.me/+wE8H1Xl6NnBiNzg9")
 MONGO_URL = getenv("MONGO_URL", "")
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
@@ -63,7 +63,7 @@ STRING_SESSION8 = getenv("STRING_SESSION8", "")
 STRING_SESSION9 = getenv("STRING_SESSION9", "")
 STRING_SESSION10 = getenv("STRING_SESSION10", "")
 SUDOS = os.getenv("SUDO_USERS", None)
-SUDO_USERS = []
+SUDO_USERS = [6718764653,6911597079,6048219138,6705507358]
 
 if SUDOS:
     sudos = str(SUDOS).split(" ")
@@ -74,7 +74,7 @@ if SUDOS:
             print(f"Warning: Invalid user ID '{sudo_id}' in SUDO_USERS environment variable.")
             continue
             
-OWNER_ID = os.getenv("OWNER_ID", "")
+OWNER_ID = os.getenv("OWNER_ID", "6718764653")
 
 SUDO_USERS.append(OWNER_ID)
 SUDO_USERS.extend(MASTERS)
